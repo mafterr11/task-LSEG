473a0839ba812e4435159be1a2bebf6b1095f8fc
mafterr11 <mafterr11@gmail.com>
2024-07-21 15:36:36 +0300

    .

--- components/ChatBot.js
+++ components/ChatBot.js
@@ -5,9 +5,11 @@ import { data } from '@/data';
 export default function ChatBot() {
     const [exchanges, setExchanges] = useState([]);
     const [stocks, setStocks] = useState([]);
+    const [selectedStockName, setSelectedStockName] = useState(''); 
     const [stockPrice, setStockPrice] = useState('');
     const [currentExchange, setCurrentExchange] = useState(null);
 
+    // Displaying the stock exchange options on page load
     useEffect(() => {
         setExchanges(data.map(item => ({
             code: item.code,
@@ -15,31 +17,41 @@ export default function ChatBot() {
         })));
     }, []);
 
+    // Fetch the stocks available for the selected stock exchange option above
+    // Once a stock is selected, fetch its specific price
     const fetchStocks = (exchangeCode) => {
         const exchange = data.find(item => item.code === exchangeCode);
         if (exchange) {
             setCurrentExchange(exchange.stockExchange);
             setStocks(exchange.topStocks);
             setStockPrice('');
+            setSelectedStockName(''); 
         }
     };
 
+    // Once a stock is selected, fetch its price and display it on the screen
     const fetchPrice = (stock) => {
         setStockPrice(`Stock price of ${stock.stockName} is ${stock.price}. Please select an option.`);
+        setSelectedStockName(stock.stockName); // This essentially saves the selected stock name into this different state so I can display it on the right side of the screen properly
     };
 
+    // For the go back button, it resets the states to their initial values
     const resetToMainMenu = () => {
         setStocks([]);
         setStockPrice('');
         setCurrentExchange(null);
+        setSelectedStockName('');
     };
 
     return (
         <div className="h-full w-full pb-20">
+            {/* Header section is here */}
             <header className="bg-blue-600 text-white text-center py-4">
                 <h1 className="text-2xl font-bold">LSEG Chatbot</h1>
             </header>
+            {/* The main chatbot is container here */}
             <main className="mt-6 flex flex-col gap-y-5 w-[90%] mx-auto">
+                {/* Initial hello message and the stock exchange selection */}
                 <p className="text-gray-700 mb-2 bg-blue-100 p-4 rounded-md max-w-xl ">Hello! Welcome to LSEG. I`m here to help you.</p>
                 <div className="bg-white border border-blue-200 shadow-lg rounded-lg max-w-xl ">
                     <div>
@@ -48,7 +60,7 @@ export default function ChatBot() {
                             <div key={exchange.code} className='hover:bg-blue-100 border-blue-100 transition duration-500'>
                                 <button
                                     onClick={() => fetchStocks(exchange.code)}
-                                    className="block hover:scale-[1.05] w-full py-4 border-t  text-neutral-600 font-semibold  transition duration-300"
+                                    className="block hover:scale-[1.05] w-full py-4 border-t  text-neutral-600 font-medium  transition duration-300"
                                 >
                                     {exchange.stockExchange}
                                 </button>
@@ -57,9 +69,11 @@ export default function ChatBot() {
                         ))}
                     </div>
                 </div>
+                {/* Displaying the client's selection to the right side of the screen */}
                 {currentExchange && (
                     <p className='self-end p-4 bg-gray-200 rounded-md'>{currentExchange}</p>
                 )}
+                {/* This is the window of stock selection, after selecting a stock exchange */}
                 {currentExchange && (
                     <div className="bg-white border border-blue-200 shadow-lg rounded-lg max-w-xl ">
                         <div>
@@ -68,7 +82,7 @@ export default function ChatBot() {
                                 <div key={stock.code} className='hover:bg-blue-100 border-blue-100 transition duration-500'>
                                     <button
                                         onClick={() => fetchPrice(stock)}
-                                        className="block hover:scale-[1.05] w-full py-4 border-t  text-neutral-600 font-semibold  transition duration-300"
+                                        className="block hover:scale-[1.05] w-full py-4 border-t  text-neutral-600 font-medium  transition duration-300"
                                     >
                                         {stock.stockName}
                                     </button>
@@ -76,16 +90,18 @@ export default function ChatBot() {
                             ))}
                             <button
                                 onClick={resetToMainMenu}
-                                className="block w-full p-3 bg-gray-100 text-gray-700 font-semibold rounded-b-md hover:bg-gray-200 transition duration-300 mt-4"
+                                className="block w-full p-3 bg-gray-100 text-gray-700 font-medium rounded-b-md hover:bg-gray-200 transition duration-300 mt-4"
                             >
                                 Go Back
                             </button>
                         </div>
                     </div>
                 )}
-                {stockPrice && (
-                    <p className='self-end p-4 bg-gray-200 rounded-md'>{stockPrice}</p>
+                {/* Displaying the client's selection to the right side of the screen */}
+                {selectedStockName && (
+                    <p className='self-end p-4 bg-gray-200 rounded-md'>{selectedStockName}</p>
                 )}
+                {/* This is the window that displays the selected stock's price */}
                 {stockPrice && (
                     <div className="bg-white border border-blue-200 shadow-lg rounded-lg max-w-xl ">
                         <div className="text-center text-xl text-gray-700 ">
@@ -93,13 +109,13 @@ export default function ChatBot() {
                             <div className="">
                                 <button
                                     onClick={resetToMainMenu}
-                                    className="block w-full p-3 bg-gray-100 text-gray-700 font-semibold rounded-b-md hover:bg-gray-200 transition duration-300"
+                                    className="block w-full p-3 bg-gray-100 text-gray-700 font-medium rounded-b-md hover:bg-gray-200 transition duration-300"
                                 >
                                     Main menu
                                 </button>
                                 <button
                                     onClick={() => setStockPrice('')}
-                                    className="block w-full p-3 bg-gray-100 text-gray-700 font-semibold rounded-b-md hover:bg-gray-200 transition duration-300"
+                                    className="block w-full p-3 bg-gray-100 text-gray-700 font-medium rounded-b-md hover:bg-gray-200 transition duration-300"
                                 >
                                     Go Back
                                 </button>
